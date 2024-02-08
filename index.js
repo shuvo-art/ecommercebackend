cb2ec6c5c1fbd8721d8242b10e0763092b9e2f04
shuvo-art <shuvo83qn@gmail.com>
2024-02-09 04:55:37 +0600

    Updated back end for promo code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 4000;
+const port = 4000;
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -6,15 +6,15 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
-const BASE_URL = process.env.BASE_URL;
-const DATABASE = process.env.DATABASE;
+//const BASE_URL = process.env.BASE_URL;
+//const DATABASE = process.env.DATABASE;
 
 app.use(express.json());
 app.use(cors());
 
 // Database connection With MongoDb
-//mongoose.connect("mongodb+srv://shuvo_art:Ixaqmeh1@cluster0.4vlukdz.mongodb.net/e-commerce");
-mongoose.connect(`${DATABASE}`);
+mongoose.connect("mongodb+srv://shuvo_art:Ixaqmeh1@cluster0.4vlukdz.mongodb.net/e-commerce");
+//mongoose.connect(`${DATABASE}`);
 
 // API Creation
 
@@ -41,7 +41,7 @@ app.use('/images', express.static('upload/images'))
 app.post('/upload', upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `${BASE_URL}/images/${req.file.filename}`
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
     })  
 })
 
@@ -127,6 +127,82 @@ app.get('/allproducts', async (req, res) => {
     let products = await Product.find({});
     console.log("All Products Fetched");
     res.json(products);
+})
+
+//schema creation for promo Code
+
+const PromoCode = mongoose.model("PromoCode", {
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        uppercase: true,
+    },
+    usages: {
+        type: Number,
+        required: true,
+    },
+    discount: {
+        type: Number,
+        required: true,
+    },
+    createdate: {
+        type: Date,
+        default: Date.now,
+    },
+    startdate: {
+        type: Date,
+    },
+    enddate: {
+        type: Date,
+    }, 
+});
+
+
+app.post('/addpromocode', async (req, res) => {
+    let promoCodes = await PromoCode.find({});
+    let id;
+    if(promoCodes.length > 0) {
+        let last_promo_array = promoCodes.slice(-1);
+        let last_promo = last_promo_array[0];
+        id = last_promo.id + 1;
+    }
+    else {
+        id = 1;
+    }
+
+    const promoCode = new PromoCode({
+        id: id,
+        name: req.body.name,
+        usages: req.body.usages,
+        discount: req.body.discount,
+        startdate: req.body.startdate,
+        enddate: req.body.enddate,
+    });
+    await promoCode.save();
+    res.json({
+        success: true,
+        name: req.body.name,
+    })
+})
+
+//Creating API for deleting Promo Code
+
+app.post('/removepromocode', async (req, res) => {
+    await PromoCode.findOneAndDelete({id: req.body.id});
+    console.log("Removed");
+    res.json({
+        success: true,
+        name: req.body.name,
+    })
+})
+
+//Creating API for getting all promo codes
+
+app.get('/allpromocodes', async (req, res) => {
+    let promoCodes = await PromoCode.find({});
+    console.log("All Products Fetched");
+    res.json(promoCodes);
 })
 
 //schema creation for user model
@@ -246,7 +322,7 @@ app.post('/removefromcart', fetchUser, async (req, res) => {
 
 //creating endpoint for getting cart data
 app.post('/getcart', fetchUser, async (req, res) => {
-    console.log("Get Cart", req.body.itemID, req.user.id);
+    console.log("Get Cart", req.user.id);
     let userData = await Users.findOne({_id: req.user.id});
     res.json(userData.cartData);
 })
